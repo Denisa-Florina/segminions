18430bd1d6014ba123a7fd6b70a0a666738ccce7
denisa05 <58482093+denisa05@users.noreply.github.com>
2025-10-29 20:43:27 +0200

    fix

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+import sys 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def create_app():
     app = Flask(__name__)
